0bd3b4ce976f0296977bf63461da63254954cc55
Abigail Collins <abigailjcollins@gmail.com>
2024-01-22 21:02:35 -0500

    JS follow along 1-10

---
+++ test.js
@@ -0,0 +1,13 @@
+// first alert
+alert("Hello, World!");
+
+// second alert
+alert ("Hello, World!");
+
+
+
+// myVar += -= *= /= with number
+// ++myVar adds 1, --myVar subtracts 1
+
+// document.write(myVar); can have variable, number, or equation in ()
+// console.log(myVar); can have variable, number, or equation in ()
